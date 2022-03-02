d926b9d87a4295654b3338c6064a5eb603d1a645
Van6242 <qhart52336@gmail.com>
2022-03-02 10:18:31 -0500

    mvp

--- Guess the number/ViewController.swift
+++ Guess the number/ViewController.swift
@@ -2,18 +2,71 @@
 //  ViewController.swift
 //  Guess the number
 //
-//  Created by  on 2/22/22.
+//  Created by  on 2/22/22 22:22.22
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
+{
+    var rand = Int.random(in: 1...100)
+        
+    
+  
+    @IBOutlet weak var hLable: UILabel!
+    @IBOutlet weak var guessfield: UITextField!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //get random number
+           
     }
-
-
+    
+    
+    
+    @IBAction func myButton(_ sender: Any)
+    {
+    //get number from textfield
+        print(rand)
+        
+        var guess = Int(guessfield.text!) ?? 0
+            print(guess)
+        
+    //compare the numbers
+        if rand < guess
+        {
+            hLable.text = "Lower"
+            
+            
+        }
+        else if rand > guess
+        {
+            hLable.text = "higher"
+        }
+        else
+        {
+            hLable.text = "CORECT!!"
+            reset()
+           
+        }
+        
+    }
+    
+    
+    func reset()
+    {
+        rand = Int.random(in: 1...100)
+        hLable.text = ""
+        
+    }
+    
 }
 
+
+import SwiftUI
+import ConfettiSwiftUI
